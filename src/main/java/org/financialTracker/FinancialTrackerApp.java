cb42eb92dce83574e1ec65463aaaf3b28cde391e
Mykyta Nikolaichuk <nikolas20052016@gmail.com>
2025-02-05 06:55:44 +0200

    change Main to FinancialTrackerApp

---
+++ src/main/java/org/financialTracker/FinancialTrackerApp.java
@@ -0,0 +1,14 @@
+package org.financialTracker;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialTrackerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialTrackerApp.class, args);
+    }
+
+}
