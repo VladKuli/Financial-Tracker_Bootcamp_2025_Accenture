c16d3e815f4bd16003fc9dc5b5d738ea3987750e
Mykyta Nikolaichuk <nikolas20052016@gmail.com>
2025-02-10 15:46:34 +0200

    creating a storage service for storing and processing utilized tokens at logout

---
+++ src/main/java/org/financialTracker/service/RevokedTokenService.java
@@ -0,0 +1,20 @@
+package org.financialTracker.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class RevokedTokenService {
+    private final Set<String> revokedTokens;
+
+    public void revokeToken(String token) {
+        revokedTokens.add(token);
+    }
+
+    public boolean isTokenRevoked(String token) {
+        return revokedTokens.contains(token);
+    }
+}
